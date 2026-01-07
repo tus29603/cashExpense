a6a0af56c4bb993608334a2b8a91d92ac6a978bd
Tesfaldet Haileab <tus29603@temple.edu>
2026-01-07 10:57:59 -0500

    Initial Commit

---
+++ cashExpense/cashExpenseApp.swift
@@ -0,0 +1,32 @@
+//
+//  cashExpenseApp.swift
+//  cashExpense
+//
+//  Created by Tesfaldet Haileab on 1/7/26.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct cashExpenseApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
